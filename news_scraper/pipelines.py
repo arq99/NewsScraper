1af4f6a091c6b8b6215e1cadde8ce68cfb9c93c7
arq99 <aqureshi1028@gmail.com>
2022-01-14 02:03:08 -0500

    created a pipeline to upload data collected to google cloud

--- news_scraper/pipelines.py
+++ news_scraper/pipelines.py
@@ -6,8 +6,14 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from google.cloud import firestore
 
 
-class NewsScraperPipeline:
+class LinksPipeline:
+
+    def __init__(self):
+        db = firestore.Client(project='my-project-id')
+
     def process_item(self, item, spider):
+        print("__________________________________________")
         return item
